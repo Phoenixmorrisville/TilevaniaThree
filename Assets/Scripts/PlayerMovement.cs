4b9c5f74758a6eeeb5dbfb17b5d8fcd2a9782b61
Phoenixmorrisville <mamrak387@morrisville.edu>
2025-11-02 17:28:05 -0500

    horizontal movement made for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     Vector2 moveInput;
+    Rigidbody2D myRigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log("Move Input: " + moveInput);
+    }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.linearVelocity.y);
+        myRigidbody.linearVelocity = playerVelocity;
     }
 }
